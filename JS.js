4fbb389e719e8b12a89334ff65e2d65fde896a85
callonlogin-ops <callonlogin@gmail.com>
2026-09-16 07:19:37 -0300

    Add files via upload

---
+++ JS.js
@@ -0,0 +1,16 @@
+var play.src=documentGetById(form.value(play.src=https://www.lojaintegrada.com.br/ =Clouds.src=sitesgoogle.src));
+var Clouds;
+Clouds=Clouds.src="https://console.cloud.google.com/auth/clients ";
+var sitesgoogle;
+sitesgoogle=sitesgoogle.src="https://sites.google.com";
+var load.src=eval(console.log(play));
+ var ip.src=console.log(load);
+var playset.src=documentGetById(form.value(playset.src=https://www.gmail.com.br/ ));
+var load.src=eval(console.log(play=playip.src=CPFnew.src));
+ var ip.src=console.log(load);
+var CPFnew;
+CPFnew.src=documentGetById(form.vaule(CPFnew.src=https://ibioseg.pi.gov.br/signin?login.xhtml
+));
+var playip.src=documentGetById(form.value(playip.src=https://rastreamento.correios.com.br/app/index.php ));
+var load.src=eval(console.log(play));
+ var ip.src=console.log(load);
