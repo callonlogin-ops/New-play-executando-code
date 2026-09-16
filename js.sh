351713075c4b9079b15429939e37d377b93dbf48
callonlogin-ops <callonlogin@gmail.com>
2026-09-16 07:19:03 -0300

    Add files via upload

---
+++ js.sh
@@ -0,0 +1,63 @@
+# PowerShell Translation
+
+$Clouds = "https://console.cloud.google.com/auth/clients"
+$sitesgoogle = "https://sites.google.com"
+$play = "https://www.lojaintegrada.com.br/"
+$playset = "https://www.gmail.com.br/"
+$CPFnew = "https://ibioseg.pi.gov.br/signin?login.xhtml"
+$playip = "https://rastreamento.correios.com.br/app/index.php"
+
+Write-Output $play
+$load = $play
+Write-Output $load
+
+Write-Output $play
+$load = $play
+Write-Output $load
+
+Write-Output $play
+$load = $play
+Write-Output $load
+r/app/index.php ));
+    $playip = "https://rastreamento.correios.com.br/app/index.php"
+    
+    # Logic from the provided PS section in the source:
+    # $play = $playip
+    $play = $playip
+    
+    # $load = $CPFnew
+    $load = $cpfNew
+    
+    # Write-Output $play
+    Write-Output $play
+    
+    # $ip = $load
+    $ip = $load
+    
+    # Write-Output $ip
+    Write-Output $ip
+}
+function Load-Data(){
+var form=createElemente(form);
+var play.src=documentGetById(form.value(play.src=https://www.lojaintegrada.com.br/ =Clouds.src=sitesgoogle.src));
+var Clouds;
+Clouds=Clouds.src="https://console.cloud.google.com/auth/clients ";
+var sitesgoogle;
+sitesgoogle=sitesgoogle.src="https://sites.google.com";
+var load.src=eval(console.log(play));
+ var ip.src=console.log(load);
+var playset.src=documentGetById(form.value(playset.src=https://www.gmail.com.br/ ));
+var load.src=eval(console.log(play=playip.src=CPFnew.src));
+ var ip.src=console.log(load);
+var CPFnew;
+CPFnew.src=documentGetById(form.vaule(CPFnew.src=https://ibioseg.pi.gov.br/signin?login.xhtml
+));
+var playip.src=documentGetById(form.value(playip.src=https://rastreamento.correios.com.br/app/index.php ));
+var load.src=eval(console.log(play));
+ var ip.src=console.log(load);
+
+}
+# console.log(Load-Data());
+$result = Load-Data
+Write-Host $result
+
