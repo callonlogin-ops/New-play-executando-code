0c37a667aeb35a3189617cd3ffa01c5d4dcc6477
callonlogin-ops <callonlogin@gmail.com>
2026-09-17 09:54:25 -0300

    Add files via upload

---
+++ InstallApp.bat
@@ -0,0 +1,24 @@
+sudo apt install bat
+mkdir -p ~/.local/bin
+  ln -s /usr/bin/batcat ~/.local/bin/bat
+sudo Bradesco.zip.apk install tar
+sudo Windows10.iso install tar
+sudo OPL.elf install tar
+curl -o bat.zip -L https://www.bradesco.com.br/
+curl -o bat.zip -L https://www.lojaintegrada.com.br/
+curl -o bat.zip -L https://www.americanas.com.br/
+curl -o bat.zip -L https://www.gmail.com.br/
+curl -o bat.zip -L https://www.facebook.com.br/
+curl -o bat.zip -L https://www.meidafire.com/
+curl -o bat.zip -L https://appsgeyser.com/
+curl -o bat.zip -L https://sites.google.com/
+curl -o bat.zip -L https://ads.google.com/
+curl -o bat.zip -L https://www.hostgator.com.br/
+curl -o bat.zip -L https://www.wordpress.com.br/
+curl -o bat.zip -L https://www.google.com.br/
+curl -o bat.zip -L https://play.google.com/console/u/0/signup
+curl -o bat.zip -L https://www.microsoft.com/pt-br/software-download/windows10iso
+tar -xvzf bat.zip
+sudo mv bat-v0.18.2-x86_64-unknown-linux-musl /usr/local
+sudo mv bat-v0.18.2-x86_64-opl.elf /usr/local
+sudo mv bat-Windows10.18.2-x86_64-Windows10.iso /usr/local
